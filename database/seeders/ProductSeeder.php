e67fadc34ae63cd6a1aaecbaa7ef9d2116551332
Andrii Snurnikov <78219181+y3Runda@users.noreply.github.com>
2022-01-02 15:43:42 +0200

    Initial commit

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $num = 30;
+        for ($i = 1; $i <= $num; $i++) {
+            DB::table('products')->insert([
+                'title' => 'Product ' . $i,
+                'price' => rand(200, 1500),
+                'in_stock' => 1,
+                'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+            ]);
+        }
+    }
+}
