6088b35662c7f71e52cdd7f2fdbb45a565c6c23b
Andrii Snurnikov <78219181+y3Runda@users.noreply.github.com>
2022-01-02 16:10:33 +0200

    Initial commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home.index');
+        $products = Product::where('id',5)->first();
+        echo $products->title;
+        /*foreach ($products as $product) {
+            echo 'Title: '.$product->title.' | '.$product->price.'$';
+            echo '<br>';
+        }*/
+        //return view('home.index');
     }
 }
